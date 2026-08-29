b67d4c399c1aacf13a08b170cfef678ac3b71e3e
developmentKen <dev.ken1@proton.me>
2026-08-29 12:13:53 +0500

    feat(auth): refactor LoginPage layout and enhance form structure

--- frontend/src/features/auth/pages/LoginPage.tsx
+++ frontend/src/features/auth/pages/LoginPage.tsx
@@ -3,9 +3,9 @@ import {
   Typography,
   Input,
   Divider,
-  Checkbox,
   Form,
   Button,
+  Checkbox,
   Grid,
 } from "antd";
 import { UserOutlined, LockOutlined, AppleFilled } from "@ant-design/icons";
@@ -20,166 +20,162 @@ const { useBreakpoint } = Grid;
 
 export function LoginPage() {
   const { loading, contextHolder, handleSubmit } = useLogin();
+
   const screens = useBreakpoint();
   const isDesktop = screens.md;
 
   return (
-    <Flex align="center" justify="center" style={{ height: "100dvh" }}>
+    <Flex gap={20}>
       {contextHolder}
 
       <Flex
+        flex={1}
         vertical
         justify="center"
-        align="flex-start"
-        style={
-          isDesktop
-            ? { flex: 0.7, padding: "0px" }
-            : { flex: 1, padding: "20px" }
-        }
+        align="center"
+        style={{
+          height: "100dvh",
+          padding: "0 20px",
+        }}
       >
-        <div
+        <Title level={2} style={{ marginBottom: "4px" }}>
+          Welcome Back
+        </Title>
+
+        <Text
+          type="secondary"
+          style={{
+            display: "block",
+            marginBottom: "32px",
+            textAlign: "center",
+          }}
+        >
+          Enter your credentials to continue to your dashboard
+        </Text>
+
+        <Form
+          layout="vertical"
           style={{
             width: "100%",
             maxWidth: "500px",
-            margin: "0 auto",
-            textAlign: isDesktop ? "left" : "center",
+            textAlign: "left",
           }}
+          onFinish={handleSubmit}
         >
-          <Title level={2} style={{ marginBottom: "4px" }}>
-            Welcome Back
-          </Title>
-
-          <Text
-            type="secondary"
-            style={{ display: "block", marginBottom: "32px" }}
+          <Form.Item
+            name="login"
+            label="Email or Phone"
+            rules={[
+              {
+                required: true,
+                message: "Please write email or phone!",
+              },
+            ]}
           >
-            Enter your credentials to continue to your dashboard
-          </Text>
+            <Input prefix={<UserOutlined />} placeholder="example@email.com" />
+          </Form.Item>
 
-          <Form
-            layout="vertical"
-            style={{ width: "100%" }}
-            onFinish={handleSubmit}
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+              {
+                min: 8,
+                message: "Password must be at least 8 characters!",
+              },
+            ]}
           >
+            <Input.Password
+              placeholder="Enter your password"
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+
+          <Flex justify="space-between" align="center">
             <Form.Item
-              name="login"
-              label="Email or Phone"
-              rules={[
-                { required: true, message: "Please write email or phone!  " },
-              ]}
+              name="remember"
+              valuePropName="checked"
+              style={{ margin: 0 }}
             >
-              <Input
-                style={{
-                  padding: "8px",
-                  backgroundColor: "#f8f8f8",
-                  borderRadius: 18,
-                }}
-                prefix={<UserOutlined />}
-                placeholder="example@email.com"
-              />
+              <Checkbox>Remember me</Checkbox>
             </Form.Item>
 
-            <Form.Item
-              name="password"
-              label="Password"
-              rules={[
-                { required: true, message: "Please input your password!" },
-                { min: 8, message: "Password must be at least 8 characters!" },
-              ]}
+            <Link to="/">Forgot password?</Link>
+          </Flex>
+
+          <Form.Item
+            style={{
+              marginTop: "10px",
+              marginBottom: "16px",
+            }}
+          >
+            <Button
+              block
+              type="primary"
+              htmlType="submit"
+              loading={loading}
+              style={{
+                padding: "18px",
+                borderRadius: 18,
+              }}
             >
-              <Input.Password
-                style={{
-                  padding: "8px",
-                  backgroundColor: "#f8f8f8",
-                  borderRadius: 18,
-                }}
-                placeholder="Enter your password"
-                prefix={<LockOutlined />}
-              />
-            </Form.Item>
+              Sign in
+            </Button>
+          </Form.Item>
 
-            <Flex justify="space-between" align="center">
-              <Form.Item name="remember" valuePropName="checked" noStyle>
-                <Checkbox>Remember me</Checkbox>
-              </Form.Item>
+          <Form.Item style={{ textAlign: "left" }}>
+            <Link to="/signup">Do not have an account?</Link>
+          </Form.Item>
 
-              <Link to={"/"}>Forgot password?</Link>
-            </Flex>
+          <Divider style={{ margin: "24px 0" }}>Or continue with</Divider>
 
-            <Form.Item style={{ marginTop: "10px", marginBottom: "16px" }}>
-              <Button
-                block
-                type="primary"
-                htmlType="submit"
-                loading={loading}
-                style={{ padding: "18px", borderRadius: 18 }}
-              >
-                Sign in
-              </Button>
-            </Form.Item>
-
-            <Form.Item style={{ textAlign: "left" }}>
-              <Link to={"/signup"}>Already have an account?</Link>
-            </Form.Item>
-
-            <Divider style={{ margin: "24px 0" }}>Or continue with</Divider>
-
-            <Flex
-              justify="space-between"
-              gap="middle"
-              style={{ width: "100%" }}
+          <Flex justify="space-between" gap="middle" style={{ width: "100%" }}>
+            <Button
+              style={{
+                flex: 1,
+                borderRadius: 12,
+              }}
+              icon={<AppleFilled style={{ color: "#000" }} />}
             >
-              <Button
-                style={{ flex: 1, borderRadius: 12 }}
-                icon={<AppleFilled style={{ color: "#000" }} />}
-              >
-                {isDesktop && <span>Apple</span>}
-              </Button>
+              {isDesktop && <span>Apple</span>}
+            </Button>
 
-              <Button
-                style={{ flex: 1, borderRadius: 12 }}
-                icon={<GoogleIcon />}
-              >
-                {isDesktop && <span>Google</span>}
-              </Button>
+            <Button
+              style={{
+                flex: 1,
+                borderRadius: 12,
+              }}
+              icon={<GoogleIcon />}
+            >
+              {isDesktop && <span>Google</span>}
+            </Button>
 
-              <Button
-                style={{ flex: 1, borderRadius: 12 }}
-                icon={<FacebookIcon />}
-              >
-                {isDesktop && <span>Faceebook</span>}
-              </Button>
-            </Flex>
-          </Form>
-        </div>
+            <Button
+              style={{
+                flex: 1,
+                borderRadius: 12,
+              }}
+              icon={<FacebookIcon />}
+            >
+              {isDesktop && <span>Facebook</span>}
+            </Button>
+          </Flex>
+        </Form>
       </Flex>
 
       {isDesktop && (
         <div
           style={{
-            flex: 0.5,
-            height: "calc(100% - 24px)",
-            margin: "12px",
-            padding: "20px",
-            paddingTop: "5%",
-            backgroundColor: "rgb(73, 41, 255)",
+            flex: 1,
+            margin: 12,
+            background: "#0082aa",
             borderRadius: 16,
-            color: "white",
-            overflow: "hidden",
           }}
-        >
-          <Title style={{ color: "inherit" }} level={2}>
-            Are You Fully Prepared for Your Next Interactive Session?
-          </Title>
-
-          <Text
-            type="secondary"
-            style={{ color: "#f6f6f6", marginTop: "12px" }}
-          >
-            Keep track of all active user sessions, monitor device activity, and
-            secure your account with a single click.
-          </Text>
-        </div>
+        />
       )}
     </Flex>
   );
