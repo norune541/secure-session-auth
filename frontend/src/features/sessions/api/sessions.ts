44aae9168835836fbc264151bbdb89e073cf6754
Ken <dev.ken1@proton.me>
2026-09-08 12:21:09 +0500

    feat(sessions): add getUserSession function

--- frontend/src/features/sessions/api/sessions.ts
+++ frontend/src/features/sessions/api/sessions.ts
@@ -1,13 +1,29 @@
 import { HTTPError } from "ky";
 import { protectedApi } from "../../../common/api/index";
 import { ClientError } from "../../../common/error/ClientError";
-import type { AllSessionsResponse } from "@repo/types";
+import type { AllSessionsResponse, Session } from "@repo/types";
 
 export const getAllUserSessions = async () => {
   try {
     const data = await protectedApi
       .get("auth/sessions")
       .json<AllSessionsResponse>();
+    return data;
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const serverResponse = err.data;
+      throw new ClientError(serverResponse);
+    }
+
+    throw err;
+  }
+};
+
+export const getUserSession = async (sessionId: string, userId: string) => {
+  try {
+    const data = await protectedApi
+      .get(`auth/sessions/${sessionId}/${userId}`)
+      .json<Session>();
     return data;
   } catch (err) {
     if (err instanceof HTTPError) {
