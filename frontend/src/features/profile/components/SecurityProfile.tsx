9b5bba0068291c58b16d6a2232d32876009ca340
Ken <dev.ken1@proton.me>
2026-09-17 10:22:22 +0500

    feat: add logout button

--- frontend/src/features/profile/components/SecurityProfile.tsx
+++ frontend/src/features/profile/components/SecurityProfile.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { Typography, Button, Layout, Divider } from "antd";
+import {
+  Typography,
+  Button,
+  Layout,
+  Divider,
+  Flex,
+  ConfigProvider,
+} from "antd";
 
 import { PasswordModal } from "./PasswordModal";
 
@@ -10,18 +17,48 @@ export function SecurityProfile() {
   const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
 
   return (
-    <Content style={{ marginLeft: 28, marginTop: 20, maxWidth: 500 }}>
+    <Content
+      style={{ marginLeft: 28, marginTop: 20, marginRight: 28, maxWidth: 500 }}
+    >
       <Title level={3}>Security</Title>
       <Divider />
-      <Title level={4} style={{ color: "black" }}>
-        Password
-      </Title>
-      <Button
-        style={{ minWidth: 300 }}
-        onClick={() => setIsPasswordModalOpen(true)}
-      >
-        Change password
-      </Button>
+
+      <Flex vertical gap={5}>
+        <Title level={4} style={{ color: "black", marginTop: 0 }}>
+          Password
+        </Title>
+        <Button
+          onClick={() => {
+            setIsPasswordModalOpen(true);
+          }}
+        >
+          Change password
+        </Button>
+        <Divider />
+        <Title level={4} style={{ color: "black", marginTop: 0 }}>
+          Logout
+        </Title>
+        <ConfigProvider
+          theme={{
+            components: {
+              Button: {
+                colorText: "red",
+                colorBorder: "red",
+
+                defaultHoverBorderColor: "#ff4d4f",
+                defaultHoverBg: "#fff1f0",
+                defaultHoverColor: "#ff4d4f",
+
+                defaultActiveColor: "#cf1322",
+                defaultActiveBorderColor: "#cf1322",
+                defaultActiveBg: "#fff1f0",
+              },
+            },
+          }}
+        >
+          <Button>Logout</Button>
+        </ConfigProvider>
+      </Flex>
       <PasswordModal
         isOpen={isPasswordModalOpen}
         onClose={() => setIsPasswordModalOpen(false)}
