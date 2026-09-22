c2c7276ee1b546c47bea6126f3073201a8c281d1
developmentKen <dev.ken1@proton.me>
2026-09-22 16:40:41 +0000

    fix: add railway backend api link

--- frontend/src/common/api/index.ts
+++ frontend/src/common/api/index.ts
@@ -1,7 +1,9 @@
 import ky from "ky";
 import type { RefreshSessionResponse } from "@repo/types";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://sessions-backend-production.up.railway.app";
 
 export const authApi = ky.create({
   baseUrl: `${API_URL}/api/`,
